d9ff02456ec45704db5f9f47e8d4e2fe625b2adb
jianglei <qq3159659202@163.com>
2026-04-28 17:37:26 +0800

    Initial commit - STM32G431 Six-Step BLDC Motor Control Project

---
+++ MDK-ARM/RTE/_G431_MY_SIX_STEP_BMEF/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'G431_MY_SIX_STEP_BMEF' 
+ * Target:  'G431_MY_SIX_STEP_BMEF' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
